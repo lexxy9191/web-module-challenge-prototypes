2ab63be4bef2a36d851246e5eeef447228ae2232
[A[Balexia turner <alexiaturner411@gmail.com>
2022-08-16 00:46:14 -0700

    20

--- index.js
+++ index.js
@@ -86,7 +86,10 @@ function Baby(name, age, favoriteToy) {
 Person.call(this, name, age);
 this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with $(this.favoriteToy)`;
+}
 
 /* 
   TASK 4
